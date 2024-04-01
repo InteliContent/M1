e7ad4e0a405f2fbb4ec97c0630517f4fb52176fc
Kizzy Terra <kizzy.terra@prof.inteli.edu.br>
2024-04-01 13:17:13 -0300

    adicionando resolucao da lista

---
+++ Semana_07/Resolucao/ex03.js
@@ -0,0 +1,11 @@
+const numero = 10;
+
+if (numero % 2 === 0) {
+  console.log("O número é par!");
+} else if (numero % 3 === 0) {
+  console.log("O número é divisível por 3!");
+} else {
+  console.log("O número é ímpar e não é divisível por 3!");
+}
+
+/* Alternativa correta: D) O código verifica se o número é par, ímpar ou divisível por 3. Se for par, exibe a mensagem "O número é par!". Se for ímpar, exibe a mensagem "O número é ímpar!". Se for divisível por 3, exibe a mensagem "O número é divisível por 3!".*/
